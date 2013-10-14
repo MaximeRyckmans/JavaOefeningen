041f1c48cb04924e3d7a856e13f25d0ba0f8b045
Maxime <java@ubuntu>
2013-10-14 21:29:03 +0200

    Updated Leraar, Quizcat, opdrachtcat and opdrachtCat classes

--- Oefening1/src/model/Leraar.java
+++ Oefening1/src/model/Leraar.java
@@ -1,5 +1,13 @@
+/**
+ * 
+ */
 package model;
 
+/**
+ * @author Maxime Ryckmans
+ * @version 1.0
+ *
+ */
 public enum Leraar {
-	
+	JoskeVermeulen, JanJanssen, SaraSamson, PietSnot, PieterPetersen
 }
---
+++ Oefening1/src/model/OpdrachtCategorie.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * @author Maxime Ryckmans
+ * @version 1.0
+ *
+ */
+public enum OpdrachtCategorie {
+	
+	rekenen, NederlandseNaam, FranseTaal, algemeneKennis
+}
