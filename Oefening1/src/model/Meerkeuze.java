7480992c42e8e4f225ee8d755a5dd463dd126b12
Davy <java@ubuntu>
2013-10-30 21:52:49 +0100

    Finished Meerkeuze

--- Oefening1/src/model/Meerkeuze.java
+++ Oefening1/src/model/Meerkeuze.java
@@ -1,15 +1,24 @@
 package model;
 
-public class Meerkeuze extends Opdracht implements Valideerbaar{
+import java.util.HashMap;
+import java.util.Map;
+
+public class Meerkeuze extends Opdracht implements Valideerbaar {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	private Map<Integer, String> keuzesMap;
+	
 	@Override
 	public boolean isJuisteAntwoord(String antwoord) {
-		// TODO Auto-generated method stub
+		int integer = Integer.parseInt(antwoord);
+		
+		if (super.getAntwoord() == keuzesMap.get(integer)) {
+			return true;
+		}
+		
 		return false;
 	}
 
@@ -27,8 +36,40 @@ public class Meerkeuze extends Opdracht implements Valideerbaar{
 
 	@Override
 	public int compareTo(Opdracht o) {
-		// TODO Auto-generated method stub
-		return 0;
+		final int BEFORE = -1;
+		final int  EQUAL = 0;
+		final int AFTER = 1;
+		
+		if(this == o) return EQUAL;
+		
+		if (super.getId() < o.getId()) return BEFORE;
+		return AFTER;
 	}
+
+	public Map<Integer, String> getKeuzesMap() {
+		return keuzesMap;
+	}
+
+	public void setKeuzesMap(Map<Integer, String> keuzesMap) {
+		this.keuzesMap = keuzesMap;
+	}
+	
+	public void samenstellenKeuzes(String alleKeuzes){
+		String delimiter = ";";
+		String[] temp = alleKeuzes.split(delimiter);
+		
+		keuzesMap = new HashMap<Integer, String>();
+		
+		for (int i = 0; i < temp.length; i++) {
+			keuzesMap.put(i++, temp[i]);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "/nMeerkeuze [keuzesMap=" + keuzesMap + "]";
+	}
+
+	
 
 }
