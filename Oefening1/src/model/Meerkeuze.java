5e70b29ca95aa61bfb559a45076ae6ad7c6eda91
Davy <java@ubuntu>
2013-10-31 10:48:29 +0100

    meerKeuze() Aangepast en vervolledigd

--- Oefening1/src/model/Meerkeuze.java
+++ Oefening1/src/model/Meerkeuze.java
@@ -11,13 +11,34 @@ public class Meerkeuze extends Opdracht implements Valideerbaar {
 	 */
 	private static final long serialVersionUID = 1L;
 	private Map<Integer, String> keuzesMap;
+	private String valideerTekst = "Nog niet gevalideerd";
+	
+	public Meerkeuze(){
+		
+	}
+	
+	public Meerkeuze(String vraag, String antwoord, int maxAantalPogingen, String antwoordHint, int maxAntwoordTijd, 
+			String alleKeuzes){
+		super(vraag, antwoord, maxAantalPogingen, antwoordHint, maxAntwoordTijd);
+		
+		samenstellenKeuzes(alleKeuzes);
+		
+		if (!keuzesMap.containsValue(antwoord)) {
+			keuzesMap.clear();
+			throw new IllegalArgumentException("De gegeven keuzes bevatten niet het juiste antwoord");
+		}
+	}
 	
 	@Override
 	public boolean isJuisteAntwoord(String antwoord) {
+		try{
 		int integer = Integer.parseInt(antwoord);
 		
 		if (super.getAntwoord() == keuzesMap.get(integer)) {
 			return true;
+		}
+		}catch(NumberFormatException nfe){
+			System.out.println("Het gegeven antwoord is geen geldig cijfer");
 		}
 		
 		return false;
@@ -25,14 +46,25 @@ public class Meerkeuze extends Opdracht implements Valideerbaar {
 
 	@Override
 	public boolean isValide(String antwoord) {
-		// TODO Auto-generated method stub
-		return false;
+		try{
+			int integer = Integer.parseInt(antwoord);
+			
+			if (integer > 0 && integer < keuzesMap.size()) {
+				this.valideerTekst = "Het ingegeven antwoord is een mogelijke keuze";
+				return true;
+			}else {
+				this.valideerTekst = "Het ingegeven antwoord behoort niet tot de juiste mogelijkheden";
+				return false;
+			}
+			}catch (NumberFormatException nfe) {
+				this.valideerTekst = "Het gegeven antwoord is geen geldig cijfer";
+				return false;
+			}
 	}
 
 	@Override
 	public String getValideerTekst() {
-		// TODO Auto-generated method stub
-		return null;
+		return valideerTekst;
 	}
 
 	@Override
