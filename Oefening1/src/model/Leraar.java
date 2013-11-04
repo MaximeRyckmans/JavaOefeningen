38b1f1125e3ae51b267d3f35c87eeab94b5947d2
Nicolas Goossens <java@ubuntu>
2013-11-04 20:25:27 +0100

    Added getters and setters for the Enum Leraar

--- Oefening1/src/model/Leraar.java
+++ Oefening1/src/model/Leraar.java
@@ -9,5 +9,33 @@ package model;
  *
  */
 public enum Leraar {
-	JoskeVermeulen, JanJanssen, SaraSamson, PietSnot, PieterPetersen
+        JoskeVermeulen("Joske", "Vermeulen"),
+        JanJanssen("Jan","Janssen"), 
+        SaraSamson("Sara","Samson"), 
+        PietSnot("Piet","Snot"), 
+        PieterPetersen("Pieter","Petersen");
+        
+        private String voorNaam;
+        private String familieNaam;
+        
+        Leraar(String voorNaam, String familieNaam){
+                this.setVoorNaam(voorNaam);
+                this.setFamilieNaam(familieNaam);
+        }
+
+        public String getVoorNaam() {
+                return voorNaam;
+        }
+
+        public void setVoorNaam(String voorNaam) {
+                this.voorNaam = voorNaam;
+        }
+
+        public String getFamilieNaam() {
+                return familieNaam;
+        }
+
+        public void setFamilieNaam(String familieNaam) {
+                this.familieNaam = familieNaam;
+        }
 }
