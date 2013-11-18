d53e63890889d81d913e3bd60d35df3fe2f660ba
Maxime <java@ubuntu>
2013-11-18 18:26:42 +0100

    Created a very, very basic window and a start class with a main() method in it.

---
+++ Oefening1/src/utils/Start.java
@@ -0,0 +1,11 @@
+package utils;
+
+import view.QuizCreatieView;
+
+public class Start {
+
+	public static void main(String[] args) {
+		QuizCreatieView view = new QuizCreatieView();
+	}
+
+}
--- Oefening1/src/view/QuizCreatieView.java
+++ Oefening1/src/view/QuizCreatieView.java
@@ -3,5 +3,33 @@ package view;
 import javax.swing.*;
 
 public class QuizCreatieView extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	public QuizCreatieView(){
+		createAndShowGUI();
+	}
+	
+	// Create the content pane which displays the buttons and widgets on-screen.
+    private JPanel createContentPane()
+    {
+        JPanel totalGUI = new JPanel();
+        totalGUI.setOpaque(true);
+        return totalGUI;
+    }
+	
+    private void createAndShowGUI()
+    {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("Quiz");
+
+        // Set the content pane.
+        frame.setContentPane(createContentPane());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
