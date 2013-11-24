3abca2d66c09c88fa72af41a737db33271246124
Maxime <java@ubuntu>
2013-11-24 20:25:51 +0100

    added constructor to controller, issues with Jtable <-- correct use?

--- Oefening1/src/controller/QuizCreatieController.java
+++ Oefening1/src/controller/QuizCreatieController.java
@@ -1,11 +1,29 @@
 package controller;
 
+import model.OpdrachtCatalogus;
 import model.Quiz;
+import model.QuizCatalogus;
 import view.QuizCreatieView;
 
 public class QuizCreatieController {
-	
+
 	private QuizCreatieView quizCreatieView;
 	private Quiz quiz;
+	private OpdrachtCatalogus opdrachtCatalogusModel;
+	private QuizCatalogus quizCatalogusModel;
+
+	public QuizCreatieController(QuizCreatieView quizCreatieView,
+			OpdrachtCatalogus opdrachtCatalogusModel,
+			QuizCatalogus quizCatalogusModel) {
+			
+		this.quizCreatieView = quizCreatieView;
+		this.opdrachtCatalogusModel = opdrachtCatalogusModel;
+		this.quizCatalogusModel = quizCatalogusModel;
+		
+		this.opdrachtCatalogusModel.leesOpdrachtenVanBestand();
+		this.quizCatalogusModel.leesQuizzenVanBestand();
+		
+	//	this.quizCreatieView.setOpdrachten(opdrachtCatalogusModel.getOpdrachten());
+	}
 
 }
