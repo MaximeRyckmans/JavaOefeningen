0d3a634ed98f40722f39927116bb575e1e232807
Davy <java@ubuntu>
2013-11-25 14:15:27 +0100

    Trying QuizListView()

--- Oefening1/src/utils/Start.java
+++ Oefening1/src/utils/Start.java
@@ -1,11 +1,12 @@
 package utils;
 
 import view.QuizCreatieView;
+import view.QuizListView;
 
 public class Start {
 
 	public static void main(String[] args) {
-		QuizCreatieView view = new QuizCreatieView();
+		QuizListView view = new QuizListView();
 	}
 
 }
