33e59636a5419d6a1992669d523a94b2ad710c66
Davy <java@ubuntu>
2013-12-09 19:30:10 +0100

    Aanmaken van QuizWijzigenController

---
+++ Oefening1/src/controller/QuizWijzigenController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.QuizCatalogus;
+import view.QuizWijzigenView;
+
+/**
+ * 
+ * @author Davy Pulinx
+ * @version 1.0
+ *
+ */
+
+public class QuizWijzigenController implements ActionListener {
+
+	private QuizWijzigenView quizWijzigenView;
+	private QuizCatalogus qCat;
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
