53843e3b1981f9c13fc658e495d8151e1c681ebf
Davy <java@ubuntu>
2013-12-14 12:38:35 +0100

    Adding QuizListController to Github, as for some reason it didn't push through

---
+++ Oefening1/src/controller/QuizListController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.OpdrachtCatalogus;
+import model.QuizCatalogus;
+import view.QuizCreatieView;
+import view.QuizListView;
+import view.QuizWijzigenView;
+
+public class QuizListController implements ActionListener{
+	
+	private QuizListView quizListView;
+	private QuizCatalogus quizCatalogus;
+	private OpdrachtCatalogus opdrachtCatalogus;
+
+	public QuizListController() {}
+	
+	public QuizListController(QuizListView quizListView, QuizCatalogus quizCatalogus, OpdrachtCatalogus opdrachtCatalogus) {
+		this.quizListView = quizListView;
+		this.quizCatalogus = quizCatalogus;
+		this.opdrachtCatalogus = opdrachtCatalogus;
+		
+		quizListView.createNodes(quizCatalogus);
+		quizListView.buttonActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("Creëer nieuwe quiz")) {
+			QuizCreatieView quizCreatieView = new QuizCreatieView();
+			QuizCreatieController quizCreatieController = new QuizCreatieController(opdrachtCatalogus, quizCatalogus, quizCreatieView);
+		}else if (e.getActionCommand().equals("Wijzigen van een quiz")) {
+			QuizWijzigenView quizWijzigenView = new QuizWijzigenView();
+			QuizWijzigenController quizWijzigenController = new QuizWijzigenController(quizWijzigenView, quizCatalogus, opdrachtCatalogus);
+		}
+	}
+}
